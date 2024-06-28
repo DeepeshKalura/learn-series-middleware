b428243a875e359632d12794902e8d973f2d8386
DeepeshKalura <deepeshkalurs@gmail.com>
2024-06-28 17:11:34 +0530

    chore: Update database URL in db.py

--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker 
 
-DB_URL = "sqlite://database.db"
+DB_URL = "sqlite:///database.db"
 engine = create_engine(
     DB_URL, connect_args= {"check_same_thread": False}
 )
